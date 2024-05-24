52e8b85cc7d415f387010a09b4fc874ebddf8547
Rafal1985 <rafael.sarnecki@gmail.com>
2024-05-24 23:41:07 +0200

    first test for reader module

--- src/test/java/pl/sarnecki/readarticleretriever/ReadArticleRetrieverFacadeTest.java
+++ src/test/java/pl/sarnecki/readarticleretriever/ReadArticleRetrieverFacadeTest.java
@@ -1,5 +1,6 @@
 package pl.sarnecki.readarticleretriever;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -7,15 +8,18 @@ import static org.junit.jupiter.api.Assertions.*;
 class ReadArticleRetrieverFacadeTest {
 
     @Test
-    public void f(){
+    @DisplayName("should return article with id 1 and content costam")
+    public void should_return_article_with_id_1_and_content_costam() {
         //give
         ReadArticleRetrieverFacade facade = new ReadArticleRetrieverFacade();
         //when
-        ArticleDto articleDto=facade.retrieveById(1L);
+        ArticleDto articleDto = facade.retrieveById(1L);
         //then
+        assert articleDto.id().equals(1L);
+        assert articleDto.header().equals("Jak zostać programistą Java");
+        assert articleDto.content().equals("5 kroków: 1, 2, 3, 4, 5");
+        assert articleDto.category().equals("IT");
+
+
     }
-
-
-
-
 }
